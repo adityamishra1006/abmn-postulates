9ef7a4ef3062d71923cf203a14bb22836bd475e1
adityamishra1006 <mishraaditya6001@gmail.com>
2024-02-19 00:31:36 +0530

    Added Hero Section

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function toggleMenu() {
     var menu = document.querySelector('.right-nav');
     menu.classList.toggle('show');
 }
+
+var typeData = new Typed(".role", {
+    strings: [
+      "Empower yourself with tech",
+      "Get In Demand Skills",
+      "Upskill yourself with new technologies",
+      "Gain Practical Training",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
